9a8243b96be952d8d83c014c8c1031732a7b9163
nofeesahdee <nofeesahdee@gmail.com>
2022-03-07 19:58:11 +0100

    update

---
+++ faq/script.js
@@ -0,0 +1,8 @@
+const faq = document.querySelectorAll('.faq')
+const toggleBtn = document.querySelectorAll('.faq-toggle')
+
+toggleBtn.forEach((btn) => {
+    btn.addEventListener('click', () =>{
+        btn.parentNode.classList.toggle('active')
+    })
+})
